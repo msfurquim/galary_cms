366c8b72adbc438b3e7678a82d817424949c6e60
luoping <luoping0425@gmail.com>
2012-10-09 21:51:36 +0800

    FEATURE - first init

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+  before_filter :set_locale
+
+  def set_locale
+    logger.debug "----params[:locale] #{params[:locale]}--------------"
+    ::I18n.locale = params[:locale] || 'en'
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GallaryCms::Application.config.secret_token = '873fe8e27ca51ee222d2fb8ff76b21cb868caf91d7c8f4a826cfa9c60110e0df38fe1ffe58dbc2e2e7d14a8d0848fa08476285fd0bd1af6600e63c18b046e8dc'
